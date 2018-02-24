7144009558a3ca4babb58a95eaf4ce3465506f4c
alex-mytko <33318927+alex-mytko@users.noreply.github.com>
2018-02-24 15:22:30 +0200

    Add files via upload

---
+++ CRC.h
@@ -0,0 +1,18 @@
+#ifndef __CRC_H__
+#define __CRC_H__
+
+#if ARDUINO >= 100
+	#include <Arduino.h>
+#else
+	#include <WProgram.h>
+#endif
+
+class CRC
+{
+public:
+	static uint8_t  getCRC8(byte* data);
+	static uint16_t getCRC16(byte* data);
+	static uint32_t getCRC32(byte* data);
+};
+
+#endif
